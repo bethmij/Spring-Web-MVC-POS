b4682f85a10a2bb263e8997ed63800b3fed201ae
bethmij <bethmij@gmail.com>
2024-04-18 19:55:38 +0530

    CustomerEntity added

---
+++ Backend/src/main/java/lk/ijse/gdse66/spring/entity/CustomerEntity.java
@@ -0,0 +1,25 @@
+package lk.ijse.gdse66.spring.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customers")
+public class CustomerEntity {
+
+    @Id
+    private String id;
+    private String name;
+    private String address;
+    @Column(columnDefinition = "LONGTEXT")
+    private String profilePic;
+
+}
