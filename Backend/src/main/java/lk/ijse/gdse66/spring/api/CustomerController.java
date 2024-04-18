431161da105ad5b11c2a779cc4464e1b3f9ce984
bethmij <bethmij@gmail.com>
2024-04-18 21:53:02 +0530

    customer controller updated

--- Backend/src/main/java/lk/ijse/gdse66/spring/api/CustomerController.java
+++ Backend/src/main/java/lk/ijse/gdse66/spring/api/CustomerController.java
@@ -1,16 +1,57 @@
 package lk.ijse.gdse66.spring.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lk.ijse.gdse66.spring.dto.CustomerDTO;
+import lk.ijse.gdse66.spring.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Base64;
+import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/customers")
+@RequestMapping("/api/v1/customers")
+@CrossOrigin
 public class CustomerController {
 
-    @GetMapping
-    public String getAllCustomers(){
-        System.out.println("Get Method Received");
-        return "Msg received";
+    @Autowired
+    CustomerService customerService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<CustomerDTO> getAllCustomers(){
+        return customerService.getAllCustomers();
+    }
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public CustomerDTO saveCustomer(@RequestPart("id") String id,
+                                    @RequestPart("name") String name,
+                                    @RequestPart("address") String address,
+                                    @RequestPart("profilePic") String profilePic){
+        String base64ProfilePic = Base64.getEncoder().encodeToString(profilePic.getBytes()); //Build Base64 image
+        CustomerDTO customer = new CustomerDTO(id, name, address, base64ProfilePic);
+        return customerService.saveCustomer(customer);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable("id") String id){
+        customerService.deleteCustomer(id);
+    }
+
+    @PatchMapping(value = "/{id}",consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCustomer(@PathVariable("id") String id,
+                               @Valid @RequestBody CustomerDTO customer){
+        customer.setId(id);
+        customerService.updateCustomer(customer);
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CustomerDTO GetCustomerDetails(@PathVariable("id") String id){
+        return customerService.getCustomerDetails(id);
     }
 }
--- Backend/src/main/java/lk/ijse/gdse66/spring/dto/CustomerDTO.java
+++ Backend/src/main/java/lk/ijse/gdse66/spring/dto/CustomerDTO.java
@@ -1,8 +1,13 @@
 package lk.ijse.gdse66.spring.dto;
 
 import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerDTO {
     private String id;
     private String name;
