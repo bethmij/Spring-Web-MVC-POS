e81b7c7da3d26eea93ac1e9c2bd2bd39bce7d300
bethmij <bethmij@gmail.com>
2024-04-20 09:39:13 +0530

    transformer updated

--- Backend/src/main/java/lk/ijse/gdse66/spring/service/util/Transformer.java
+++ Backend/src/main/java/lk/ijse/gdse66/spring/service/util/Transformer.java
@@ -2,8 +2,12 @@ package lk.ijse.gdse66.spring.service.util;
 
 import lk.ijse.gdse66.spring.dto.CustomerDTO;
 import lk.ijse.gdse66.spring.dto.ItemDTO;
+import lk.ijse.gdse66.spring.dto.OrderDTO;
+import lk.ijse.gdse66.spring.dto.OrderDetailsDTO;
 import lk.ijse.gdse66.spring.entity.Customer;
 import lk.ijse.gdse66.spring.entity.Item;
+import lk.ijse.gdse66.spring.entity.Order;
+import lk.ijse.gdse66.spring.entity.OrderDetails;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
@@ -30,5 +34,21 @@ public class Transformer {
 
     public Item toItemEntity(ItemDTO itemDTO){
         return mapper.map(itemDTO, Item.class);
+    }
+
+    public OrderDTO fromOrderEntity(Order order){
+        return mapper.map(order, OrderDTO.class);
+    }
+
+    public Order toOrderEntity(OrderDTO orderDTO){
+        return mapper.map(orderDTO, Order.class);
+    }
+
+    public OrderDetailsDTO fromOrderDetailEntity(OrderDetails orderDetails){
+        return mapper.map(orderDetails, OrderDetailsDTO.class);
+    }
+
+    public OrderDetails toOrderDetailEntity(OrderDetailsDTO orderDetailsDTO){
+        return mapper.map(orderDetailsDTO, OrderDetails.class);
     }
 }
